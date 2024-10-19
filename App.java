1d339c670ff3c1a1613133f878597129bf6dfb48
Ville-Mikko Hakamäki <villemikko.hakamaki94@gmail.com>
2024-10-19 19:22:23 +0300

    Initial commit

---
+++ App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       System.out.println("Hei olen tulostin ohjelma");
+       String tekija = "Ville-Mikko Hakamäki";
+       System.out.print("Ohjelman tekijä: ");
+       System.out.println(tekija);
+       //Lukujen määritelmät
+       double luku1;
+       double luku2;
+       double tulo;
+       double tulo2;
+       double tulo3;
+       double tulo4;
+       luku1 = 7;
+       luku2 = 2;
+       //Muuttujien arvot
+       System.out.println(luku1);
+       System.out.println(luku2);
+       System.out.println("luku1-muuttujan arvo on " + luku1);
+       System.out.println("luku2-muuttujan arvo on " + luku2);
+       //Laskutoimitukset
+        tulo = luku1 * luku2;
+        System.out.println("luku1 * luku2 = " + tulo);
+        tulo2 = luku1 / luku2;
+        System.out.println("luku1 / luku2 = " + tulo2);
+        tulo3 = luku1 + luku2;
+        System.out.println("luku1 + luku2 = " + tulo3);
+        tulo4 = luku1 - luku2;
+        System.out.println("luku1 - luku2 = " + tulo4);
+    }          
+}
